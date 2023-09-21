791c180918db539f0dece7bc312e0c856e7745ce
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-09-21 22:24:33 +0300

    add login and logout pages

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView, LoginView
 from main.views import index, signup
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('signup/', signup, name='signup')
+    path('signup/', signup, name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='main/login.html'), name='login')
 ]
